497353e53c47901e6c7a0ae940087bc832f872a5
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-10-11 18:09:49 +0900

    Remove protobufs from update-codegen.sh

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -27,33 +27,6 @@ boilerplate="${REPO_ROOT_DIR}/hack/boilerplate/boilerplate.go.txt"
 
 echo "=== Update Codegen for $MODULE_NAME"
 
-# Parse flags to determine if we should generate protobufs.
-generate_protobufs=0
-while [[ $# -ne 0 ]]; do
-  parameter=$1
-  case ${parameter} in
-    --generate-protobufs) generate_protobufs=1 ;;
-    *) abort "unknown option ${parameter}" ;;
-  esac
-  shift
-done
-readonly generate_protobufs
-
-if (( generate_protobufs )); then
-  group "Generating protocol buffer code"
-  protos=$(find "${REPO_ROOT_DIR}/pkg" "${REPO_ROOT_DIR}/test" -name '*.proto')
-  for proto in $protos
-  do
-    protoc "$proto" -I="${REPO_ROOT_DIR}" --gogofaster_out=plugins=grpc:.
-
-    # Add license headers to the generated files too.
-    dir=$(dirname "$proto")
-    base=$(basename "$proto" .proto)
-    generated="${dir}/${base}.pb.go"
-    echo -e "$(cat "${boilerplate}")\n\n$(cat "${generated}")" > "${generated}"
-  done
-fi
-
 group "Gateway API Codegen"
 
 # Gateway API

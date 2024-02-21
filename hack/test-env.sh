92a9d2a57caa52009be11fb3165c1fb39456fcbe
Izabela Gomes <igomes@vmware.com>
2024-02-21 16:54:53 -0500

    Bump Contour to v1.28.1 (#625)
    * bump Contour version to 1.28.1
    
    * run ./hack/update-codegen.sh

--- hack/test-env.sh
+++ hack/test-env.sh
@@ -17,5 +17,5 @@
 export GATEWAY_API_VERSION="v0.8.1"
 export ISTIO_VERSION="1.19.3"
 export ISTIO_UNSUPPORTED_E2E_TESTS="retry,httpoption"
-export CONTOUR_VERSION="v1.26.0"
+export CONTOUR_VERSION="v1.28.1"
 export CONTOUR_UNSUPPORTED_E2E_TESTS="httpoption,basics/http2,grpc,grpc/split,update"
